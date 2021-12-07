3d077c0e480b2b95a00cc5fade6585e82465afc9
Matt Keane <mattpkeane@gmail.com>
2021-12-07 17:24:26 -0600

    completed day06 part 2

---
+++ day06/part2/app.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    const fishes = data.split(',').map(fish => +fish)
+    let fish = fishes.reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1
+        return acc
+    }, {})
+    
+    for (let i = 0; i < 256; i ++) {
+        const newFish = {}
+        for (const key in fish) {
+            const day = +key
+            if (day === 0) {
+                newFish[8] = (newFish[8] || 0) + fish[0]
+                newFish[6] = (newFish[6] || 0) + fish[0]
+            } else {
+                newFish[day - 1] = (newFish[day - 1] || 0) + fish[day]
+            }
+        }
+        fish = newFish
+    }
+
+    let count = 0
+    for (const key in fish) {
+        count += fish[key]
+    }
+
+    console.log(count)
+})
